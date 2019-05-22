d6e476b39b8a1f93130d47a5d33815cc007ee14a
Victoria <bernard.victoria22@gmail.com>
2019-05-22 13:22:50 -0500

    Init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+/**
+ * readRequestBody reads in the incoming request body
+ * Use await readRequestBody(..) in an async function to get the string
+ * @param {Request} request the incoming request to read from
+ */
+async function readRequestBody(request) {
+  const { headers } = request
+  const contentType = headers.get('content-type')
+
+  if (contentType.includes('application/json')) {
+    const body = await request.json()
+    return JSON.stringify(body)
+  } else if (contentType.includes('application/text')) {
+    const body = await request.text()
+    return body
+  } else if (contentType.includes('text/html')) {
+    const body = await request.text()
+    return body
+  } else {
+    const body = await request.body()
+    return body
+  }
+}
+
+
+
